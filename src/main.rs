f289f1e06159eeb3e6d6c420a10fdbca5ca1f0b5
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2024-02-28 19:34:30 +0100

    code rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,271 +1,43 @@
 mod util;
 
+use std::borrow::BorrowMut;
 use std::collections::{HashMap, HashSet};
 use std::fmt::Display;
-use std::hash::Hasher;
+use std::hash::{Hash, Hasher};
 use std::path::Path;
-use std::str::FromStr;
-use std::time::SystemTime;
-use std::{fs, path::PathBuf};
+use std::path::PathBuf;
 
+use anyhow::Result;
 use chrono::{DateTime, TimeZone, Utc};
 use dialoguer::console::Term;
 use dialoguer::{theme::ColorfulTheme, FuzzySelect};
-use metrohash::MetroHash;
-use rand::seq::SliceRandom;
 use rand::{thread_rng, Rng};
-use rusqlite::types::ValueRef;
-use rusqlite::{params, Connection, Row};
-use serde::{Deserialize, Serialize};
+use sqlx::{query, Connection, SqliteConnection};
+use tokio::fs;
 use util::path_str;
 use walkdir::WalkDir;
 
 // TODO: warn when file is found which is marked as deleted in db. maybe you don't want to revive
 // it.
 
-#[derive(Debug, Clone, Default, Deserialize, Serialize)]
-pub struct FileInfo {
-    comment: Option<String>,
-
-    #[serde(default)]
-    tags: HashSet<String>,
-
-    #[serde(flatten)]
-    rest: HashMap<String, serde_yaml::Value>,
-}
-
-#[derive(Debug)]
-pub struct EntryOrdering {
-    left_path: String,
-    right_path: String,
-    vote: i64,
-    at: DateTime<Utc>,
-}
-
-#[derive(Debug, Clone)]
-pub struct HashChange {
-    hash: u64,
-    at: DateTime<Utc>,
-}
-
-#[derive(Debug, Clone)]
-pub struct File {
-    path: PathBuf,
-    info: Option<FileInfo>,
-    hash_changes: Vec<HashChange>,
-    _hash: u64,
-    score: i64,
-}
-
-impl File {
-    pub fn last_hash(&self) -> Option<u64> {
-        self.hash_changes.last().map(|h| h.hash)
-    }
-}
-
-impl Display for File {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self.path)
-    }
-}
-
-fn get_files() -> Vec<File> {
-    WalkDir::new("entries")
-        .into_iter()
-        .filter_map(|entry| {
-            let entry = entry.unwrap();
-            if !entry.file_type().is_file() {
-                return None;
-            }
-
-            let path = entry.into_path();
-
-            let s = fs::read_to_string(&path).unwrap();
-
-            let hash = {
-                let mut hasher = MetroHash::new();
-                hasher.write(s.as_bytes());
-                hasher.write_u8(0xff);
-                hasher.finish()
-            };
-
-            let info = {
-                let mut lines = s.lines();
-                if let Some("---") = lines.next() {
-                    let yaml: String = lines.take_while(|l| *l != "---").collect();
-                    Some(serde_yaml::from_str(&yaml).unwrap())
-                } else {
-                    None
-                }
-            };
-
-            Some(File {
-                path,
-                info,
-                hash_changes: vec![],
-                _hash: hash,
-                score: 0,
-            })
-        })
-        .collect()
-}
-
-fn get_db_files(conn: &mut Connection) -> Vec<File> {
-    let mut b = conn
-        .prepare("SELECT path FROM entries WHERE deleted = FALSE")
-        .unwrap();
-    let entries = b
-        .query_map(params![], |r: &Row<'_>| -> Result<String, _> { r.get(0) })
-        .unwrap();
-
-    let mut hash_stmt = conn
-        .prepare("SELECT hash, at FROM hash_changes WHERE path = ?1 ORDER BY at ASC")
-        .unwrap();
-
-    let mut items: HashMap<String, File> = entries
-        .map(|entry| {
-            let path = entry.unwrap();
-
-            let hash_changes: Result<Vec<_>, _> = hash_stmt
-                .query_map(params![path], |r| {
-                    Ok(HashChange {
-                        hash: {
-                            let s: String = r.get(0)?;
-                            u64::from_str_radix(&s, 16).unwrap()
-                        },
-                        at: Utc.timestamp_opt(r.get(1)?, 0).unwrap(),
-                    })
-                })
-                .unwrap()
-                .collect();
-
-            (
-                path.clone(),
-                File {
-                    path: PathBuf::from(path),
-                    info: None,
-                    hash_changes: hash_changes.unwrap(),
-                    _hash: 0,
-                    score: 0,
-                },
-            )
-        })
-        .collect();
-
-    let mut c = conn
-        .prepare("SELECT left_path, right_path, vote, at FROM entry_votes")
-        .unwrap();
-    let orderings = c
-        .query_map(params![], |r| {
-            Ok(EntryOrdering {
-                left_path: r.get(0)?,
-                right_path: r.get(1)?,
-                vote: r.get(2)?,
-                at: Utc.timestamp_opt(r.get(3)?, 0).unwrap(),
-            })
-        })
-        .unwrap();
-
-    for ordering in orderings {
-        let ordering = ordering.unwrap();
-
-        items.get_mut(&ordering.left_path).unwrap().score -= ordering.vote;
-        items.get_mut(&ordering.right_path).unwrap().score += ordering.vote;
-    }
-
-    let mut res: Vec<_> = items.into_iter().map(|p| p.1).collect();
-    res.sort_by_key(|i| i.score);
-    res
-}
-
-fn set_db_files(conn: &mut Connection) {
-    let fs_files = get_files();
-    let db_files = get_db_files(conn, Ordering::None);
-
-    let tx = conn.transaction().unwrap();
-
-    for f in db_files {
-        if fs_files.iter().any(|fsf| fsf.path == f.path) {
-            continue;
-        }
-
-        let path = path_str(&f.path);
-
-        tx.execute(
-            r#"
-            DELETE FROM entries
-            WHERE path = ?1
-        "#,
-            params![path],
-        )
-        .unwrap();
-    }
-
-    let mut exists_stmt = tx
-        .prepare("SELECT COUNT(*)>0 FROM entries WHERE path = ?1")
-        .unwrap();
-    let mut prev_hash_stmt = tx
-        .prepare("SELECT hash FROM hash_changes WHERE path = ?1 ORDER BY at DESC LIMIT 1")
-        .unwrap();
-
-    for f in fs_files {
-        let ts = Utc::now().timestamp();
-        let path = path_str(&f.path);
-
-        let exists = exists_stmt
-            .query_row(params![path], |r| Ok(r.get::<_, i64>(0)? == 1))
-            .unwrap();
-
-        if !exists {
-            tx.execute(
-                r#"
-                INSERT INTO entries
-                    (path, deleted)
-                VALUES
-                    (?1, ?2)
-                "#,
-                params![path, false],
-            )
-            .unwrap();
-        }
-
-        let prev_hash: Result<Vec<u64>, _> = prev_hash_stmt
-            .query_map(params![path], |r| {
-                let s: String = r.get(0)?;
-                Ok(u64::from_str_radix(&s, 16).unwrap())
-            })
-            .unwrap()
-            .collect();
-        let prev_hash = prev_hash.unwrap().into_iter().next();
-
-        let hash_same = prev_hash.is_some_and(|h| f._hash == h);
-        if !hash_same {
-            tx.execute(
-                r#"
-                INSERT INTO hash_changes
-                    (path, hash, at)
-                VALUES
-                    (?1, ?2, ?3)
-                "#,
-                params![path, format!("{:x}", f._hash), ts],
-            )
-            .unwrap();
-        }
-    }
-
-    drop(exists_stmt);
-    drop(prev_hash_stmt);
-    tx.commit().unwrap();
-}
-
-fn competition(conn: &mut Connection, winner: &Path, loser: &Path) {
+async fn competition(conn: &mut SqliteConnection, winner: &Path, loser: &Path) -> Result<()> {
     assert!(winner != loser);
 
-    conn.execute(
+    let loser = path_str(loser);
+    let winner = path_str(winner);
+    let score = 1;
+    let ts = Utc::now().timestamp();
+
+    query!(
         "INSERT INTO entry_votes VALUES (?1, ?2, ?3, ?4)",
-        params![path_str(loser), path_str(winner), 1, Utc::now().timestamp()],
+        loser,
+        winner,
+        score,
+        ts
     )
-    .unwrap();
+    .execute(conn)
+    .await?;
+    Ok(())
 }
 
 fn take_n_random<T>(rng: &mut impl Rng, items: &mut Vec<T>, n: usize) -> Vec<T> {
@@ -283,20 +55,132 @@ fn take_n_random<T>(rng: &mut impl Rng, items: &mut Vec<T>, n: usize) -> Vec<T> 
     res
 }
 
-fn update_files(conn: &mut Connection) {
-    let mut get_db_file_stmt = conn
-        .prepare(
-            r#"
-            SELECT
-                path,
-                deleted,
-                () AS updated_at
-            FROM entries
-            WHERE path = ?1
-        "#,
-        )
-        .unwrap();
+#[derive(Debug, Clone)]
+pub struct FileContent {
+    content: Vec<u8>,
+    at: DateTime<Utc>,
+}
 
+#[derive(Debug, Clone)]
+pub struct Vote {
+    left_path: PathBuf,
+    right_path: PathBuf,
+    vote: i64,
+    at: DateTime<Utc>,
+}
+
+#[derive(Debug, Clone)]
+pub struct File {
+    path: PathBuf,
+    deleted: bool,
+    file_contents: Vec<FileContent>,
+    score: i64,
+}
+
+impl File {
+    fn last_content(&self) -> &FileContent {
+        self.file_contents
+            .last()
+            .expect("file_contents can't be empty")
+    }
+}
+
+impl Display for File {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let content = &self
+            .file_contents
+            .last()
+            .expect("file_contents can't be empty")
+            .content;
+        let s = String::from_utf8(content.clone()).unwrap();
+        let line = s.lines().nth(0).unwrap();
+        write!(f, "{} ({:?})", line, self.path)
+    }
+}
+
+impl PartialEq for File {
+    fn eq(&self, other: &Self) -> bool {
+        self.path == other.path
+    }
+}
+impl Eq for File {}
+
+impl Hash for File {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.path.hash(state)
+    }
+}
+
+async fn get_db_files(conn: &mut SqliteConnection) -> Result<Vec<File>> {
+    let items = query!(
+        r#"
+            SELECT path, deleted
+            FROM entries
+            WHERE deleted = 0
+        "#
+    )
+    .map(|r| File {
+        path: PathBuf::from(r.path),
+        deleted: r.deleted,
+        file_contents: vec![],
+        score: 0,
+    })
+    .fetch_all(conn.borrow_mut())
+    .await?;
+
+    let mut m = HashMap::with_capacity(items.len());
+    for mut item in items {
+        let item_path = item.path.to_str().unwrap();
+        let contents = query!(
+            r#"
+                SELECT content, at
+                FROM file_contents
+                WHERE path = ?1
+                ORDER BY at ASC
+            "#,
+            item_path,
+        )
+        .map(|r| FileContent {
+            content: r.content,
+            at: Utc.timestamp(r.at, 0),
+        })
+        .fetch_all(conn.borrow_mut())
+        .await?;
+
+        item.file_contents = contents;
+
+        m.insert(item.path.clone(), item);
+    }
+
+    let orderings = query!(
+        r#"
+            SELECT left_path, right_path, vote, at
+            FROM entry_votes
+            WHERE
+                (SELECT COUNT(*) FROM entries WHERE path = left_path AND deleted = 0) > 0 AND
+                (SELECT COUNT(*) FROM entries WHERE path = right_path AND deleted = 0) > 0
+        "#
+    )
+    .map(|r| Vote {
+        left_path: PathBuf::from(r.left_path),
+        right_path: PathBuf::from(r.right_path),
+        vote: r.vote,
+        at: Utc.timestamp(r.at, 0),
+    })
+    .fetch_all(conn.borrow_mut())
+    .await?;
+
+    for ordering in orderings {
+        m.get_mut(&ordering.left_path).unwrap().score -= ordering.vote;
+        m.get_mut(&ordering.right_path).unwrap().score += ordering.vote;
+    }
+
+    let mut res: Vec<_> = m.into_iter().map(|p| p.1).collect();
+    res.sort_by_key(|i| i.score);
+    Ok(res)
+}
+
+async fn update_files(conn: &mut SqliteConnection) -> Result<()> {
     let entries = WalkDir::new("entries").into_iter().filter_map(|entry| {
         let entry = entry.unwrap();
         if !entry.file_type().is_file() {
@@ -306,6 +190,9 @@ fn update_files(conn: &mut Connection) {
         Some(entry)
     });
 
+    let db_files = get_db_files(conn).await?;
+    let mut left: HashSet<&File> = db_files.iter().collect();
+
     for entry in entries {
         let metadata = entry.metadata().unwrap();
         let modified: DateTime<Utc> = metadata.modified().unwrap().into();
@@ -313,55 +200,91 @@ fn update_files(conn: &mut Connection) {
         let path = entry.path();
         let path_str = path.to_str().unwrap();
 
-        let db_file = {
-            let db_files: Result<Vec<_>, _> = get_db_file_stmt
-                .query_map(params![path_str], |r| {
-                    Ok((r.get::<_, String>(0)?, r.get::<_, bool>(1)?))
-                })
-                .unwrap()
-                .collect();
-            db_files.unwrap().into_iter().next()
-        };
+        let db_file = db_files.iter().find(|f| f.path == path);
+
+        match db_file {
+            None => {
+                query!(
+                    r#"
+                    INSERT INTO entries
+                        (path, deleted)
+                    VALUES
+                        (?1, ?2)
+                    "#,
+                    path_str,
+                    false
+                )
+                .execute(conn.borrow_mut())
+                .await?;
+            }
+            Some(db_file) => {
+                left.remove(&db_file);
+
+                let outdated = modified > db_file.last_content().at;
+                if !outdated {
+                    continue;
+                }
+            }
+        }
+
+        let bytes = fs::read(&path).await?;
+
+        match db_file {
+            Some(f) if f.last_content().content == bytes => continue,
+            None | Some(_) => {
+                let ts = Utc::now().timestamp();
+
+                query!(
+                    r#"
+                    INSERT INTO file_contents
+                        (path, content, at)
+                    VALUES
+                        (?1, ?2, ?3)
+                    "#,
+                    path_str,
+                    bytes,
+                    ts
+                )
+                .execute(conn.borrow_mut())
+                .await?;
+            }
+        }
     }
+
+    for db_file in left {
+        let path = path_str(&db_file.path);
+        query!("UPDATE entries SET deleted = 1 WHERE path = ?1", path)
+            .execute(conn.borrow_mut())
+            .await?;
+    }
+
+    Ok(())
 }
 
-fn main() {
-    for f in get_files() {
-        println!("{:?}", f);
-    }
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mut rng = thread_rng();
+    let mut conn = SqliteConnection::connect("./db.db").await?;
 
-    let mut conn = Connection::open("./db.db").unwrap();
+    update_files(&mut conn).await?;
 
-    set_db_files(&mut conn);
-
-    //let mut rng = thread_rng();
-
-    let items = get_db_files(&mut conn, Ordering::Checked);
+    let mut items = get_db_files(&mut conn).await?;
+    let items = take_n_random(&mut rng, &mut items, 2);
 
     let selection = FuzzySelect::with_theme(&ColorfulTheme::default())
-        .items(&items[0..2])
+        .items(&items)
         .default(0)
         .interact_on_opt(&Term::stderr())
         .unwrap()
         .unwrap();
 
     let other = [1, 0][selection];
-    competition(&mut conn, &items[selection].path, &items[other].path);
+    competition(&mut conn, &items[selection].path, &items[other].path).await?;
 
-    println!("{:?} {:?}", items[0], items[1]);
-
-    let items = get_db_files(&mut conn, Ordering::Checked);
-    for f in items {
-        println!("{:?}", f);
+    let items = get_db_files(&mut conn).await?;
+    for item in items.into_iter().rev() {
+        println!("{} (score: {})", item, item.score);
     }
 
-    return;
-    /*
-
-    for f in get_files() {
-        println!("{:?}", f);
-    }
-
-    set_db_files(&mut conn);
-    */
+    Ok(())
 }
